e4bf4ed3e3d0f9934022d890fc27cb0fc4253195
Ivan Pchelnikov <pchelnikoviv@yandex.ru>
2020-09-25 15:31:50 +0400

    shit that doesn't work

--- src/main/java/ru/pchelnikov/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/ru/pchelnikov/SpringBootDemo/SpringBootDemoApplication.java
@@ -1,13 +1,18 @@
 package ru.pchelnikov.SpringBootDemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "ru.pchelnikov.SpringBootDemo")
-public class SpringBootDemoApplication {
 
+public class SpringBootDemoApplication {
+	private final static Logger logger = LoggerFactory.getLogger(SpringBootDemoApplication.class);
 	public static void main(String[] args) {
+		logger.info("main method started");
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+		logger.info("main method ended");
 	}
 
 }
