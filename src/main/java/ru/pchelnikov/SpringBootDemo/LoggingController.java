107d1cd71fd4707b09b66e6b318e04c28125144e
Ivan Pchelnikov <pchelnikoviv@yandex.ru>
2020-09-28 09:29:55 +0400

    added new class

---
+++ src/main/java/ru/pchelnikov/SpringBootDemo/LoggingController.java
@@ -0,0 +1,23 @@
+package ru.pchelnikov.SpringBootDemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoggingController {
+    Logger logger = LoggerFactory.getLogger(LoggingController.class);
+
+    @RequestMapping("/")
+    public String index() {
+        logger.trace("TRACE message");
+        logger.debug("DEBUG message");
+        logger.info("INFO message");
+        logger.warn("WARN message");
+        logger.error("ERROR message");
+
+        return "Check your logs in console and file...";
+    }
+}
