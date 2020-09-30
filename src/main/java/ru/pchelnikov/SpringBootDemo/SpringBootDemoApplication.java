fb4883ac041b6437b59b43dbdbe54569acdefa4c
Ivan Pchelnikov <pchelnikoviv@yandex.ru>
2020-09-30 10:05:31 +0400

    added context

--- src/main/java/ru/pchelnikov/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/ru/pchelnikov/SpringBootDemo/SpringBootDemoApplication.java
@@ -3,14 +3,22 @@ package ru.pchelnikov.SpringBootDemo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.pchelnikov.SpringBootDemo.Handlers.ProxyHandler;
 
 @Slf4j
 @SpringBootApplication(scanBasePackages = "ru.pchelnikov.SpringBootDemo")
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
+		log.info("Trying to bypass proxy, if it exists");
+		ProxyHandler.bypassProxy();
 
-		log.info("Enough with children games, starting the SpringBoot app");
+		log.info("Creating application context");
+		ApplicationContext context = new AnnotationConfigApplicationContext("ru.pchelnikov.SpringBootDemo");
+
+		log.info("Starting the SpringBoot application");
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 
