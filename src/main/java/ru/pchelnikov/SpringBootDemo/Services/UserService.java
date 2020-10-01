940c66aa3d0f174e723522f1ac88a8978d72459a
Ivan Pchelnikov <pchelnikoviv@yandex.ru>
2020-10-01 14:35:23 +0400

    changed ArrayList USER_LIST to HashMap USER_MAP (in UserService)

--- src/main/java/ru/pchelnikov/SpringBootDemo/Services/UserService.java
+++ src/main/java/ru/pchelnikov/SpringBootDemo/Services/UserService.java
@@ -3,10 +3,12 @@ package ru.pchelnikov.SpringBootDemo.Services;
 import lombok.extern.slf4j.Slf4j;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 public class UserService {
-    private static final ArrayList<User> USER_LIST = new ArrayList<>();
+    private static final Map<Long, User> USER_MAP = new HashMap<>();
 
     /**
      * creates new User and adds him to USER_LIST if
@@ -21,12 +23,12 @@ public class UserService {
         );
         log.info("User {} has been created", user.getUserName());
 
-        if (!USER_LIST.contains(user)) {
-            USER_LIST.add(user);
+        if (!USER_MAP.containsKey(user.getChatId())) {
+            USER_MAP.put(user.getChatId(), user);
             log.info("User {} has been added to userList!", user.getUserName());
         } else {
             log.warn("userList already contains user {}!", user.getUserName());
         }
-        log.info("userList now consists of: " + USER_LIST.toString());
+        log.info("userList now consists of: " + USER_MAP.values().toString());
     }
 }
