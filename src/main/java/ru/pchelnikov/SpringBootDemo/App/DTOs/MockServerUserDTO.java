3aaa661c1309f65507161c032444c9108943e0ce
zoodes <pchelnikoviv@yandex.ru>
2020-10-31 16:28:39 +0400

    [VDO-9011] changed id type in MockServerUserDTO: now it is UUID, not String

--- src/main/java/ru/pchelnikov/SpringBootDemo/App/DTOs/MockServerUserDTO.java
+++ src/main/java/ru/pchelnikov/SpringBootDemo/App/DTOs/MockServerUserDTO.java
@@ -1,12 +1,24 @@
 package ru.pchelnikov.SpringBootDemo.App.DTOs;
 
-import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import java.util.Date;
+import java.util.UUID;
+//TODO delete Lombok annotations
+
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MockServerUserDTO {
     public Date birthDay;
     public Long chatId;
     public String firstName;
-    public String id;
+//    public String id;
+    public UUID id;
     public Boolean male;
     public String middleName;
     public String phone;
