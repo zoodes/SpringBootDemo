32d8dda65669df923662a9fbbba6b68f074b1f59
zoodes <pchelnikoviv@yandex.ru>
2020-11-01 15:55:31 +0400

    [VDO-9015] added MockServerMockServiceClient

---
+++ src/main/java/ru/pchelnikov/SpringBootDemo/App/Clients/MockServerMockServiceClient.java
@@ -0,0 +1,70 @@
+package ru.pchelnikov.SpringBootDemo.App.Clients;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.pchelnikov.SpringBootDemo.App.DTOs.MockServerUserDTO;
+import ru.pchelnikov.SpringBootDemo.ServicesInterfaces.IMockServerServiceClient;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+
+@Slf4j
+@Service
+public class MockServerMockServiceClient implements IMockServerServiceClient {
+
+    private final Map<UUID, MockServerUserDTO> UuidToDto = new HashMap<>();
+
+    @PostConstruct
+    private void init() {
+        UUID id = UUID.randomUUID();
+        MockServerUserDTO dto = new MockServerUserDTO();
+        dto.birthDay = new Date();
+        dto.chatId = "123456";
+        dto.firstName = "firstName";
+        dto.middleName = "middleName";
+        dto.secondName = "secondName";
+        dto.id = id;
+        dto.male = true;
+        dto.phone = "123456";
+        UuidToDto.put(id, dto);
+    }
+
+    @Override
+    public UUID create(MockServerUserDTO mockServerUserDTO) {
+        UUID id = UUID.randomUUID();
+
+        MockServerUserDTO mockServerUserDTO1 = new MockServerUserDTO();
+        mockServerUserDTO1.birthDay = mockServerUserDTO.birthDay;
+        mockServerUserDTO1.chatId = mockServerUserDTO.chatId;
+        mockServerUserDTO1.firstName = mockServerUserDTO.firstName;
+        mockServerUserDTO1.middleName = mockServerUserDTO.middleName;
+        mockServerUserDTO1.secondName = mockServerUserDTO.secondName;
+        mockServerUserDTO1.id = id;
+        mockServerUserDTO1.male = mockServerUserDTO.male;
+        mockServerUserDTO1.phone = mockServerUserDTO.phone;
+
+        UuidToDto.put(id, mockServerUserDTO1);
+        return id;
+    }
+
+    @Override
+    public MockServerUserDTO read(UUID id) {
+        return UuidToDto.get(id);
+    }
+
+    @Override
+    public List<MockServerUserDTO> readAll() {
+        return new ArrayList<>(UuidToDto.values());
+    }
+
+    @Override
+    public boolean hasUser(String phone) {
+        return UuidToDto.values().stream()
+                .anyMatch(dto -> phone.equals(dto.phone));
+    }
+
+    @Override
+    public boolean hasUser(UUID id) {
+        return UuidToDto.containsKey(id);
+    }
+}
