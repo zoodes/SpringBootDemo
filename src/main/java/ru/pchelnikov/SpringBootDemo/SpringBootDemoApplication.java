c5a365d93a74a98c6fce3563f86e362f5079c121
zoodes <pchelnikoviv@yandex.ru>
2020-11-01 16:57:51 +0400

    [VDO-9017] useMock in working state

--- src/main/java/ru/pchelnikov/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/ru/pchelnikov/SpringBootDemo/SpringBootDemoApplication.java
@@ -4,25 +4,51 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.telegram.telegrambots.ApiContextInitializer;
+import ru.pchelnikov.SpringBootDemo.App.DTOs.MockServerUserDTO;
 import ru.pchelnikov.SpringBootDemo.ServicesInterfaces.IMockServerServiceClient;
+
+import java.util.Date;
+import java.util.UUID;
 
 @Slf4j
 @SpringBootApplication(scanBasePackages = "ru.pchelnikov.SpringBootDemo")
 public class SpringBootDemoApplication {
 
-	@Autowired
-	private IMockServerServiceClient mockServerServiceClient;
+//	@Autowired
+//	private IMockServerServiceClient mockServerServiceClient;
 
 	public static void main(String[] args) {
 		log.info("Initializing TelegramBot");
 		ApiContextInitializer.init();
 
 		log.info("Starting the SpringBoot application");
-		SpringApplication.run(SpringBootDemoApplication.class, args);
+//		SpringApplication.run(SpringBootDemoApplication.class, args);
 
 
-		SpringBootDemoApplication s = new SpringBootDemoApplication();
-		log.debug("readAll(): {}", s.mockServerServiceClient.readAll());
+		ApplicationContext ctx = SpringApplication.run(SpringBootDemoApplication.class, args);
+
+		IMockServerServiceClient mockServerServiceClient = ctx.getBean(IMockServerServiceClient.class);
+		log.debug("readAll(): {}", mockServerServiceClient.readAll());
+		log.debug("add new user...");
+		UUID id = UUID.randomUUID();
+		MockServerUserDTO dto = new MockServerUserDTO();
+		dto.birthDay = new Date();
+		dto.chatId = "123456789";
+		dto.firstName = "Ivan";
+		dto.middleName = "Ivanovich";
+		dto.secondName = "Ivanov";
+		dto.id = id;
+		dto.male = true;
+		dto.phone = "+79641831346";
+		UUID newId = mockServerServiceClient.create(dto);
+		log.debug("success! new user id is: {}", newId);
+		log.debug("now user list is: {}", mockServerServiceClient.readAll());
+		log.debug("has user with newId: {}", mockServerServiceClient.hasUser(newId));
+		log.debug("read user with newId: {}", mockServerServiceClient.read(newId));
+		log.debug("has user with random id: {}", mockServerServiceClient.hasUser(UUID.randomUUID()));
+		log.debug("has user with phone +79641831346: {}", mockServerServiceClient.hasUser("+79641831346"));
+		log.debug("read non-existing user: {}", mockServerServiceClient.read(UUID.randomUUID()));
 	}
 }
